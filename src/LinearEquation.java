e55c064643350e07e8716d1f77d5477c8773841b
Alex <alexbelenky07@gmail.com>
2023-10-16 17:57:15 -0400

    Finished returnFormula() code.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,13 +1,24 @@
 public class LinearEquation {
-    private int Coord1FirstInt;
-    private int Coord1SecondInt;
-    private int Coord2FirstInt;
-    private int Coord2SecondInt;
+    private int Coord1X;
+    private int Coord1Y;
+    private int Coord2X;
+    private int Coord2Y;
 
     public LinearEquation(int cord11int, int cord12int, int cord21int, int cord22int) {
-        this.Coord1FirstInt = cord11int;
-        this.Coord1SecondInt = cord12int;
-        this.Coord2FirstInt = cord21int;
-        this.Coord2SecondInt = cord22int;
+        this.Coord1X = cord11int;
+        this.Coord1Y = cord12int;
+        this.Coord2X = cord21int;
+        this.Coord2Y = cord22int;
+    }
+
+    public String returnFormula() {
+        int m = ((Coord2Y - Coord1Y) / (Coord2X - Coord1X));
+        int b = (Coord1Y - (2 * (Coord1X)));
+        if (b != 0) {
+            String formula = "y = " + m + "x" + " + " + b;
+            return formula;
+        }
+        String formula = "y = " + m + "x";
+        return formula;
     }
 }
