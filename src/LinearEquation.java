7d822d519cbe0c53f21d088aa04bea545575cb93
BT_2N2_25 <bths@sample.com>
2023-10-31 13:20:25 -0400

    working on remaning methods to match project necessities

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -11,29 +11,40 @@ public class LinearEquation {
         this.Coord1Y = cord12int;
         this.Coord2X = cord21int;
         this.Coord2Y = cord22int;
-        m = ((double) (Coord2Y - Coord1Y) /  (Coord2X - Coord1X));
-        b = (Coord1Y - (m * Coord1X));
+        slope();
+        yIntercept();
     }
 
     public void lineInfo() {
         System.out.println("\nThe two points are: (" + Coord1X + ", " + Coord1Y + ") and (" + Coord2X + ", " + Coord2Y + ")");
-        System.out.println("The equation of the line between these points is: " + returnFormula());
+        System.out.println("The equation of the line between these points is: " + equation());
         System.out.println("The slope of this line is: " + m);
         System.out.println("The y-intercept of the line is: " + b);
-        System.out.println("The distance between the two points is: " + distanceBetweenPoints());
+        System.out.println("The distance between the two points is: " + distance());
         double pairWithX = Double.parseDouble(LinearEquationLogic.askQuestion("\nEnter a value for x: "));
         System.out.println("\nThe point on the line is " + returnCoordWithX(pairWithX));
     }
 
-    public String returnFormula() {
-        if (b != 0) {
+    public String equation() {
+        if (b != 0 && b > 0) {
             return "y = " + m + "x" + " + " + b;
+        } else if (b != 0) {
+            double absB = Math.abs(b);
+            return "y = " + m + "x" + " - " + absB;
         }
         return "y = " + m + "x";
     }
 
-    public double distanceBetweenPoints() {
+    public double distance() {
         return (double) Math.round(((Math.sqrt(Math.pow((Coord2X - Coord1X), 2) + Math.pow((Coord2Y - Coord1Y), 2))) * 100)) / 100;
+    }
+
+    public void yIntercept() {
+        b = (Coord1Y - (m * Coord1X));
+    }
+
+    public void slope() {
+        m = ((double) (Coord2Y - Coord1Y) /  (Coord2X - Coord1X));
     }
 
     public String returnCoordWithX(double x) {
